20d1e5b56d8abc6943da3564202392bd17295120
waheng chang <wahengchang@gmail.com>
2019-07-13 14:02:12 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,7 @@ rm function.zip
 # zip function.zip lambda_function.py 
 
 # zip packages
-cd venv/lib/python3.7/site-packages/
-zip -r9 ${OLDPWD}/function.zip .
-
-# zip handler
-cd $OLDPWD
-zip -g function.zip lambda_function.py 
+zip -r9 ./function.zip .
 
 echo "[INFO] all file is zip, going to upload to AWS S3"
 aws lambda update-function-code --function-name helloworld-python  --zip-file fileb://function.zip
